2047c623632b131b89c1dfd247080df94ff9bfc8
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-10 17:39:40 -0700

    program that prints the ASCII alphabet, in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
-    if i % 2:
-        i = i - 32
-        print("{:c}".format(i), end="")
+    print("{:c}".format(i), end="")
+    if i % 2 == 0:
+        i -= 32
