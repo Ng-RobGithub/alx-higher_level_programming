a8e434cf0b08630fe63fdf5266234f83e35aca56
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-10 18:30:42 -0700

    prints the last digit of a number.Update..

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,3 @@ def print_last_digit(number):
     last_digit = abs(number) % 10  # Calculate the last digit
     print(last_digit, end="")
     return last_digit
-
-# Test the function
-num = 12345
-result = print_last_digit(num)
-print("\nThe last digit is:", result)
