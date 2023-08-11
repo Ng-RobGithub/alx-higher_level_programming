f4704b05aaa6d5d9ca83b7f0e81bdea65ef5a67d
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-10 17:51:42 -0700

    program that prints the ASCII alphabet update...

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print("{:c}".format(i), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        i -= 32
+        print(chr(i), end="")
+    else:
+        print(chr(i - ord('a') + ord('A')), end="")
