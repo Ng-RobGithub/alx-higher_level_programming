f6833d147f575d4a78763364dfa78c45cbc07a28
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-14 15:36:53 -0700

    Update Task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
-            if i != 0:
-                print(" ", end="")
-                print("{:d}".format(num), end="")
-                print()
+            if i == len(row) - 1:
+                print("{:d}".format(num))
+            else:
+                print("{:d}".format(num), end=" ")
