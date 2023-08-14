b80436a465a9d7f1a9331b8cb8acfcbaa5d43dc9
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-14 12:56:36 -0700

    a function that replaces an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        new_list = my_list.copy()
-        new_list[idx] = element
-        return (new_list)
-    else:
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
         return (my_list)
