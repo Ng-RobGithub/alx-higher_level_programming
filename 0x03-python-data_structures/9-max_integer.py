34d6352401c9713357ad488aad2651b44bb5d49d
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-14 19:08:22 -0700

    Update Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return (None)
-    max_num = my_list[0]
-    for num in my_list:
-        if num > max_num:
-            max_num = num
-            return (max_num)
+    new_list = []
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
