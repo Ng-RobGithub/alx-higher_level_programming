198ea78acf735cc7b60daa13b0e3b6899fe955e3
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-16 13:40:28 -0700

    computes the square value of all integers of a matrix.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,10 +4,4 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = [x ** 2 for x in row]
         new_matrix.append(new_row)
-        return (new_matrix)#!/usr/bin/python3
-    def square_matrix_simple(matrix=[]):
-        new_matrix = []
-        for row in matrix:
-            new_row = [x ** 2 for x in row]
-            new_matrix.append(new_row)
-            return (new_matrix)
+        return (new_matrix)
