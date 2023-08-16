9b3f038fe4c6fb8183cd04e81d527772665e5443
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-15 17:52:38 -0700

    Update Task 5

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx >= 0 and idx < len(my_list):
-        new_list = []
-        for i, item in enumerate(my_list):
-            if i != idx:
-                new_list.append(item)
-                return (new_list)
-            return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    new_list = [elem for i, elem in enumerate(my_list) if i != idx]
+    return (new_list)
--- 0x03-python-data_structures/11-main.py
+++ 0x03-python-data_structures/11-main.py
@@ -5,3 +5,4 @@ delete_at = __import__('11-delete_at').delete_at
         idx = 3
         new_list = delete_at(my_list, idx)
         print(new_list)
+        print(my_list)
