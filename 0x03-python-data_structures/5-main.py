8349a69ce9e6a38787458c1f2693faecdccab9c3
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-15 18:22:08 -0700

    Update Task 5

--- 0x03-python-data_structures/5-main.py
+++ 0x03-python-data_structures/5-main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 no_c = __import__('5-no_c').no_c
 
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+result1 = no_c("Best School")
+print(result1)
+result2 = no_c("Chicago")
+print(result2)
+result3 = no_c("C is fun!")
+print(result3)
