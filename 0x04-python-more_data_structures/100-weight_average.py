67360a2e9bcd1a8238e4284c694bb7a680c5d74d
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-16 19:40:45 -0700

    returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
-        return 0
+        return (0)
 
-    num = 0
-    den = 0
+    total_score = 0
+    total_weight = 0
 
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
 
-        return (num / den)
+        return (total_score / total_weight)
