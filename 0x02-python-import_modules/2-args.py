6388740ab4360b5ee47b75ccefb09c55af4db983
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-20 16:29:20 -0700

    prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    argc = len(argv)
+    size = len(sys.argv) - 1
 
-    print("Number of argument{}:".format("s" if argc != 1 else ""), argc, end="")
-    print("." if argc == 0 else ":")
-        
-        for idx, arg in enumerate(argv, start=1):
-            print("{}: {}".format(idx, arg))
+    if size == 0:
+        print("0 arguments.")
+    elif size == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(size))
+
+        for i in range(size):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
