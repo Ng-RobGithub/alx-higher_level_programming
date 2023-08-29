2bed491c7e574b948307984305cffd4122eec9a9
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-29 14:04:52 -0700

    class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-main.py
+++ 0x06-python-classes/1-main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Square module."""
+Square = __import__('1-square').Square
 
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
 
-class Square:
-    """Defines a square."""
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
 
-    def __init__(self, size):
-        """Constructor.
-        Args:
-        size: length of side of the square.
-        """
-        self.__size = size
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
