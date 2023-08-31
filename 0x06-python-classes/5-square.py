bedd17b7bd6a62b8e32c8e527c341846710b5401
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-30 17:31:06 -0700

    update task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
+
 class Square:
+    """Initialize a new square.
+
+    Args:
+    size (int): The size of the new square.
+    """
     def __init__(self, size=0):
-        self.size = size
+        self.__size = size
 
-            @property
-            def size(self):
-                return self.__size
+        @property
+        def size(self):
+            """Retrieve the size of the square."""
+            return self.__size
 
-            @size.setter
-            def size(self, value):
-                if not isinstance(value, int):
-                    raise TypeError("size must be an integer")
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                self.__size = value
+        @size.setter
+        def size(self, value):
+            """Set the size of the square.
 
-                    def area(self):
-                        return self.__size ** 2
+            Args:
+            value (int): The new size value.
+            """
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
 
-                    def my_print(self):
-                        if self.size == 0:
-                            print()
-                            return
-                        for _ in range(self.size):
-                            print("#" * self.size)
+            def area(self):
+                """Calculate the area of the square."""
+                return self.__size ** 2
+
+            def my_print(self):
+                """Print the square with the character #."""
+                if self.__size == 0:
+                    print()
+
+                else:
+                for _ in range(self.__size):
+
+                    print("#" * self.__size)
