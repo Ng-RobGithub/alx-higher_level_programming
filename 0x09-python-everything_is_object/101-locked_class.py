4e610049cf22c910a7a90b3007717240a0123355
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-05 18:35:51 -0700

    Task21 to 34

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """Defines a locked class."""
-
-
-class LockedClass:
-    """
-    Prevent the user from instantiating new LockedClass attributes
-    for anything but 'first_name'.
-    """
-
-    __slots__ = ["first_name"]
+lass LockedClass:
+    def __setattr__(self, name, value):
+        if name == "first_name":
+        self.__dict__[name] = value
+        else:
+        raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
 
