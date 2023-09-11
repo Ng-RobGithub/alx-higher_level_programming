58cb0682e2c51ab1221f2656b68502aec7e2c218
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-11 15:40:20 -0700

    a function that returns the list of available attributes and methods of an object:

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines an object attribute lookup function."""
 
-
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+    """ Get the list of attributes and methods of the object"""
+
+    attributes_and_methods = dir(obj)
+    return (attributes_and_methods)
