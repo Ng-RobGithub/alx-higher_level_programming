e8df30cd4c5cc69f491c24da24dc5dbd30f29961
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-12 08:06:14 -0700

    a function that returns True if the object is an instance of a class, otherwise false

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,16 +14,3 @@ def inherits_from(obj, a_class):
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     return False
-
-# Example usage:
-class BaseClass:
-    pass
-
-class ChildClass(BaseClass):
-    pass
-
-obj = ChildClass()
-
-print(inherits_from(obj, BaseClass))  # True, as obj is an instance of ChildClass, which inherited from BaseClass
-print(inherits_from(obj, object))      # True, as all classes inherit from the object class
-print(inherits_from(obj, int))         # False, as obj is not an instance of int
