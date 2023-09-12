52fe1f8c2e330cfb35d16cca7da4de16011df6ab
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-12 10:57:21 -0700

    update Task9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
+        """Intialize a new Rectangle."""
 
-        Args:
-        width (int): The width of the new Rectangle.
-        height (int): The height of the new Rectangle.
-        """
-        super().integer_validator("width", width)
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
 
         def area(self):
-            """Return the area of the rectangle."""
-            return (self.__width * self.__height)
+            return self.__width * self.__height
+        """Return the area of the rectangle."""
 
         def __str__(self):
             """Return the print() and str() representation of a Rectangle."""
-            string = "[" + str(self.__class__.__name__) + "] "
-
-            return (string)
+            return (f"[Rectangle] {self.__width}/{self.__height}")
