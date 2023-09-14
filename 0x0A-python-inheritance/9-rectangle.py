62bd6d7ad5decaa83d619deb4ef9e5af432c4315
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-13 19:58:53 -0700

    task 11 to 13

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,28 +2,26 @@
 """Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class Rectangle(BaseGeometry):
+class BaseGeometry:
     """Represent a rectangle using BaseGeometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
 
-    def __init__(self, width, height):
-        """Intialize a new Rectangle.
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
-        Args:
-        width (int): The width of the new Rectangle.
-        height (int): The height of the new Rectangle.
-        """
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
+        class Rectangle(BaseGeometry):
+            def __init__(self, width, height):
+                self.__width = width
+                self.__height = height
+                self.integer_validator("width", width)
+                self.integer_validator("height", height)
 
-        def area(self):
-            """Return the area of the rectangle."""
-            return self.__width * self.__height
+                def area(self):
+                    return self.__width * self.__height
 
-        def __str__(self):
-            """Return the print() and str() representation of a Rectangle."""
-            string = "[" + str(self.__class__.__name__) + "] "
-            string += str(self.__width) + "/" + str(self.__height)
-            return string
+                def __str__(self):
+                    return f"[Rectangle] {self.__width}/{self.__height}"
