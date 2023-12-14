a2082faa16fb0e60ba2663b4df00a92b802a2196
Ng-RobGithub <ngrob4real@gmail.com>
2023-12-14 12:07:37 -0800

    delete 0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++
@@ -1,6 +0,0 @@
---Script lists all genres from hbtn_0d_tvshows and the number of shows linked to each
-SELECT genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-ORDER BY number_of_shows DESC;
