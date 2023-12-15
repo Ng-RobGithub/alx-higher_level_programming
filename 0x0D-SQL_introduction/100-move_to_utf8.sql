f32e1c631be924a80c22c41cce0b8047c79c74dc
Ng-RobGithub <ngrob4real@gmail.com>
2023-12-14 21:04:19 -0800

    Advanced Task100 correction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,9 @@
--- Script converts the entire database hbtn_0c_0 to UTF8.
+-- Create and use the hbtn_0c_0 database
+-- Create the first_table table
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
