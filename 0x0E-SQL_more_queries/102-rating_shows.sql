f391dc8d4e77023efb16dac7e97b807383985f3c
Ng-RobGithub <ngrob4real@gmail.com>
2023-12-14 20:04:21 -0800

    Advanced Task 102 and 103

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
--- lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Script lists all shows from hbtn_0d_tvshows_rate by their rating
 -- lists all rows of a table by the sum of a linked row
-SELECT title, SUM(tv_show_ratings.rate) as rating_sum
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
