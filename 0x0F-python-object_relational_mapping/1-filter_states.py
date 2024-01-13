c8d59007a13c5e32dc418ef9e830790d0fdf9ede
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-13 14:26:10 -0800

    lists all states with a name starting with N (upper N) from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    """ Execute the SQL query to get states starting with 'N' """
     cur.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
     """ Fetch all the rows """
