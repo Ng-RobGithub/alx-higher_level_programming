26cec667e6ce3d7ec7468395f2f3c749893478a1
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-13 14:35:10 -0800

    lists all states with a name starting with N (upper N) from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,6 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
+    """ Connect to MySQL server """
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
