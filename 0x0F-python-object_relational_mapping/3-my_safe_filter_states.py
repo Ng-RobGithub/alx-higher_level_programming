70c018b36e771a21ecfc4a52a11c2d1a7ebe7cf9
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 14:33:29 -0800

    takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument2

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  Displays all values in the states from the database hbtn_0e_0_usa """
+""" list all values in the states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
