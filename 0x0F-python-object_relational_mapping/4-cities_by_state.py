8bfd57ef12823344e67d36f0a4f6b6675c8a505c
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 14:50:05 -0800

    lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     """ Connect to MySQL server """
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    """ Create a cursor object to interact with the database """
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id""")
