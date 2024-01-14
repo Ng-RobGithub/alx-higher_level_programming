afacf6b7543e53d9c0a79a4d2d3a83d818ab48ac
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 15:17:39 -0800

    takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,14 +5,20 @@ import sys
 
 
 if __name__ == "__main__":
+    """ Connect to MySQL server """
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    """ Create a cursor object to interact with the database """
     cur = db.cursor()
+    """ Execute the query to get cities of the given state """
     cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
+    """ Fetch all the rows """
     rows = cur.fetchall()
+    """ Display the results """
     tmp = list(row[0] for row in rows)
     print(*tmp, sep=", ")
+    """ Close cursor and database connection """
     cur.close()
     db.close()
