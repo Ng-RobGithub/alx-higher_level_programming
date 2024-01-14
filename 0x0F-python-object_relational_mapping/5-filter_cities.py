871d79500156f82013a9f8af580e010636ac1945
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 15:09:12 -0800

    takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""   Script that lists all cities of a given state from the database hbtn_0e_4_usa """
+""" lists all cities of a given state from the database hbtn_0e_4_usa """
 import MySQLdb
 import sys
 
