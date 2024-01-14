aefb5f2181c4dabcc1e5efd96f590b254aea0d30
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 14:01:46 -0800

    takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     """ Connect to MySQL server """    
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    """ Create a cursor object to interact with the database """
     cur = db.cursor()
     match = sys.argv[4]
     cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
