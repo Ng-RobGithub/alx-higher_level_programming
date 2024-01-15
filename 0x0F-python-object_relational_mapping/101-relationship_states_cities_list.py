3f51e9aef6c6439b67aec7d457665b135e8397fd
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 17:37:56 -0800

    Advanced 101 update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" lists all the State object with the name passed as argument from the database """
+""" lists all the State object with the name passed as argument from the database
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
