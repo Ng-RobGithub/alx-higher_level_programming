421ad581586aa65155427f24979b8bd1e5df717f
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-14 17:20:31 -0800

    lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" lists all the State object with the name passed as argument from the database """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -9,18 +10,12 @@ from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-    """ Set up connection to the database """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    """ Bind the database """
     Base.metadata.create_all(engine)
-    """ Create a session to interact with the database """
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    """ Display the results """
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
         for city_ins in instance.cities:
-            print("    ", end="")
-            print(city_ins.id, city_ins.name, sep=": ")
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
