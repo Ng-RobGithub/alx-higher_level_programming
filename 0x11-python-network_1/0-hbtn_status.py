4258e33231461dadd6eb4b62e5b2de22c27a377b
Ng-RobGithub <ngrob4real@gmail.com>
2024-01-26 13:38:37 -0800

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
         body = response.read()
         print("Body response:")
         print("\t- type:", type(body))
-        print("\t- content:", body.decode('utf-8'))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
