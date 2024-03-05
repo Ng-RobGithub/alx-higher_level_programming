08b6fcbd921938db6dde8d147a00c90030607e79
Ng-RobGithub <ngrob4real@gmail.com>
2024-03-04 16:00:26 -0800

    JavaScript script that fetches and lists

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+        type: 'GET',
+        success: function(data) {
+            $.each(data.results, function(index, movie) {
+                $('#list_movies').append('<li>' + movie.title + '</li>');
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error('Request failed:', error);
+        }
+    });
+});
